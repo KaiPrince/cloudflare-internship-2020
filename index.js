019e2bb0629780a77d7e89a89641c58343f8ce18
KaiPrince <34746763+KaiPrince@users.noreply.github.com>
2020-04-16 20:50:28 -0400

    Requirements completed.

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+/*
+ * Project Name: cloudflare-internship-2020
+ * File Name: index.js
+ * Programmer: Kai Prince
+ * Date: Thu, Apr 16, 2020
+ * Description: This file contains a simple Worker which returns the response
+ *  from a randomly-chosen web page.
+ */
+
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
+
 /**
- * Respond with hello worker text
+ * Respond with a random variant page.
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  const variantsUrl =
+    "https://cfw-takehome.developers.workers.dev/api/variants";
+
+  const urls = await getUrls(variantsUrl);
+
+  const randomUrl = getRandomItem(urls);
+
+  const response = await fetch(randomUrl);
+  return response;
+}
+
+async function getUrls(variantsUrl) {
+  const responseJson = await (await fetch(variantsUrl)).json();
+  const variants = responseJson.variants;
+  return variants;
+}
+
+function getRandomItem(array) {
+  const index = getRandomIntInclusive(0, Array(array).length);
+  return array[index];
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
 }
