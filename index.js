bb501b7b7801034a20585441b3a58a1c4450aac1
KaiPrince <34746763+KaiPrince@users.noreply.github.com>
2020-04-16 21:13:58 -0400

    Cleanup.

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
  *  from a randomly-chosen web page.
  */
 
+import { getVariantUrls, getRandomItem } from "./utils";
+
 addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event.request));
 });
@@ -25,35 +27,4 @@ async function handleRequest(request) {
 
   const response = await fetch(randomUrl);
   return response;
-}
-
-/**
- * Consumes a url string, and returns an array of url strings.
- * @param {string} variantsUrl The url of the variants api.
- */
-async function getVariantUrls(variantsUrl) {
-  const responseJson = await (await fetch(variantsUrl)).json();
-  const variants = responseJson.variants;
-  return variants;
-}
-
-/**
- * Returns an item from an array, selected at random.
- * @param {Array} array
- */
-function getRandomItem(array) {
-  const index = getRandomIntInclusive(0, Array(array).length);
-  return array[index];
-}
-
-/**
- * Returns a random number between a min and max, inclusive.
- * Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
- * @param {number} min The minimum range
- * @param {number} max The maximum range
- */
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
 }
